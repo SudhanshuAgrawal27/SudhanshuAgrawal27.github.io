0be0179c3a21cc9f23e95e47c500f4d657385e6b
Sudhanshu Agrawal <sudhanshuagrawal2001@gmail.com>
2021-12-28 20:29:02 +0530

    set up initial css and react for the title page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 
-const title = <h1> Under Construction! </h1>; 
 
+class Title extends React.Component 
+{
+  render()
+  {
+    return (
+      <div className="main-title">
+        <h1> Sudhanshu Agrawal </h1>
+      </div>
+    );
 
+  }
+}
 
 ReactDOM.render(
-    <h1>Under Construction!! </h1>,
+    <Title />,
     document.getElementById('root')
   );
